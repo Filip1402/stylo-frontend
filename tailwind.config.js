d88e0cc5edb9dad0415fcdd0386660a7abde657d
Martin Friščić <martin.friscic6@gmail.com>
2023-10-31 18:21:52 +0100

    Add tailwind color palette and required images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "blue-dark": "#1443BB",
+      "blue-middle": "#5F83DF",
+      "white-light": "#F8F8F8",
+      "grey-middle": "BEBEBE",
+      "grey-dark": "343434",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
