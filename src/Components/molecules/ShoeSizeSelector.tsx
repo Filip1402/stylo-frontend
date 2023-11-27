3e488d74b3866f21ebb819eebdf400aad16d6747
Martin Friščić <martin.friscic6@gmail.com>
2023-11-27 20:14:19 +0100

    Add shoe size selector component

--- src/Components/molecules/ShoeSizeSelector.tsx
+++ src/Components/molecules/ShoeSizeSelector.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./ShoeSizeSelector.scss";
 
-const ShoeSizeSelector = () => {
-  return <div>ShoeSizeSelector</div>;
+interface ShoeSizeSelectorProps {
+  setSelectedShoeSizes: React.Dispatch<React.SetStateAction<number[]>>;
+}
+
+const ShoeSizeSelector: React.FC<ShoeSizeSelectorProps> = ({
+  setSelectedShoeSizes,
+}) => {
+  const [selectedSizes, setSelectedSizes] = useState<number[]>([]);
+
+  const handleSizeChange = (size: number) => {
+    setSelectedSizes((prevSizes) =>
+      prevSizes.includes(size)
+        ? prevSizes.filter((item) => item !== size)
+        : [...prevSizes, size]
+    );
+  };
+
+  // Hardcoded 20 buttons for testing
+  const hardcodedSizes = Array.from({ length: 20 }, (_, index) => index + 1);
+
+  useEffect(() => {
+    setSelectedShoeSizes(selectedSizes);
+  }, [selectedSizes, setSelectedShoeSizes]);
+
+  return (
+    <div className="shoe-size-selector-wrapper">
+      {hardcodedSizes.map((size) => (
+        <div
+          key={size}
+          className={`shoe-size-selector-item ${
+            selectedSizes.includes(size) ? "active" : ""
+          }`}
+          onClick={() => handleSizeChange(size)}
+        >
+          {size}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ShoeSizeSelector;
--- src/Pages/ProductList.tsx
+++ src/Pages/ProductList.tsx
@@ -1,12 +1,19 @@
-import React from "react";
 import ColorItemSelector from "../Components/molecules/ColorItemSelector";
 import ShoeSizeSelector from "../Components/molecules/ShoeSizeSelector";
+import { useState } from "react";
 
 const ProductList = () => {
+  const [selectedShoeSizes, setSelectedShoeSizes] = useState<number[]>([]);
+
+  const handleSendShoeNumbers = () => {
+    console.log("Selected Shoe Sizes:", selectedShoeSizes);
+    //add API logic here
+  };
   return (
     <div>
       <ColorItemSelector />
-      <ShoeSizeSelector />
+      <ShoeSizeSelector setSelectedShoeSizes={setSelectedShoeSizes} />
+      <button onClick={handleSendShoeNumbers}>send shoe numbers</button>
     </div>
   );
 };
