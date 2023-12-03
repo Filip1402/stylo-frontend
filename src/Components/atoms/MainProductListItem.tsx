7451e091e2fadc322e5108e35a22142a4887a09f
Ana Škarica <ana.skarica.as@gmail.com>
2023-12-03 23:42:15 +0100

    Add no image image on product list items

--- src/Components/atoms/MainProductListItem.tsx
+++ src/Components/atoms/MainProductListItem.tsx
@@ -8,17 +8,25 @@ const MainProductListItem: FC<{ product: Product }> = ({ product }) => {
     console.log("Ja dobijem proizvod", product);
   });
 
-  const imageSource =
-    product.images && product.images[0] && product.images[0][0];
+  const handleError: React.ReactEventHandler<HTMLImageElement> = (event) => {
+    event.currentTarget.onerror = null; // prevents looping
+    event.currentTarget.src = NoImage;
+  };
+
   return (
     <Link to="/">
-      <div className="bg-white w-full p-7">
+      <div className="bg-white w-full p-4 md:p-7">
         <div className="bg-white h-256 w-full mb-4">
-          {imageSource ? (
+          {product ? (
             <img
-              src={imageSource}
+              src={
+                product.images && product.images[0]
+                  ? product.images[0][0]
+                  : NoImage
+              }
               alt="product image"
               className="object-contain h-full w-full"
+              onError={handleError}
             />
           ) : (
             <img
@@ -32,11 +40,11 @@ const MainProductListItem: FC<{ product: Product }> = ({ product }) => {
           <p className="text-lg text-grey-dark">{product.manufacturer}</p>
           <h3 className="text-xl text-grey-darker">{product.model}</h3>
           {product.available ? (
-            <h4 className="text-green-custom text-xl">Dostupno</h4>
+            <h4 className="text-green-custom text-md md:text-xl">Dostupno</h4>
           ) : (
-            <h4 className="text-red-500 text-xl">Nedostupno</h4>
+            <h4 className="text-red-500 text-md md:text-xl">Nedostupno</h4>
           )}
-          <h2 className="text-3xl font-bold text-grey-dark">
+          <h2 className="text-xl md:text-3xl font-bold text-grey-dark">
             {product.price} €
           </h2>
         </div>
