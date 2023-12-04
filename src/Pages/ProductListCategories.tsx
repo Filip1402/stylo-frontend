c68955a47de4cd457e2c546f8b10913c47e2dea1
Martin Friščić <martin.friscic6@gmail.com>
2023-12-04 16:45:24 +0100

    Add loaders where they were missing

--- src/Pages/ProductListCategories.tsx
+++ src/Pages/ProductListCategories.tsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getProductCategories } from "../api/product_categories";
 import { ProductCategory } from "../common/types";
+import { ThreeDots } from "react-loader-spinner";
 
 const ProductListCategories = () => {
   const [productCategories, setProductCategories] = useState<
@@ -27,11 +28,21 @@ const ProductListCategories = () => {
 
   return (
     <div>
-      <div className="flex flex-col gap-6 max-w-[100rem] mx-auto p-4 lg:grid lg:grid-cols-4 md:grid md:grid-cols-2">
-        {productCategories?.length === 0 ? (
-          <h1 className="w-full">Nema kategorija</h1>
-        ) : (
-          productCategories?.map((footware, index) => (
+      {productCategories === undefined ? (
+        <ThreeDots
+          height="80"
+          width="80"
+          radius="9"
+          color="#5F83DF"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{ justifyContent: "center" }}
+          visible={true}
+        />
+      ) : productCategories?.length === 0 ? (
+        <h1 className="max-w-7xl mx-auto pt-4">Nema kategorija</h1>
+      ) : (
+        <div className="flex flex-col gap-6 max-w-[100rem] mx-auto p-4 lg:grid lg:grid-cols-4 md:grid md:grid-cols-2">
+          {productCategories?.map((footware, index) => (
             <Link
               to={`/proizvodi/${type}/${footware.name
                 .slice(0, -1)
@@ -40,9 +51,9 @@ const ProductListCategories = () => {
             >
               <FootwareType footwareType={footware} />
             </Link>
-          ))
-        )}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
