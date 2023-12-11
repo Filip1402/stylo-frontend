60e3f98bd6817af38fb7719b3f9cb98833225218
Martin Friščić <martin.friscic6@gmail.com>
2023-12-11 17:10:06 +0100

    Add contact form validation, rewrite contact form code

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -4,37 +4,98 @@ import Input from "../Components/atoms/Input";
 import Textarea from "../Components/atoms/Textarea";
 import { notifyFailure, notifySuccess } from "../Components/atoms/Toast";
 import emailjs from "@emailjs/browser";
+import * as Yup from "yup";
 
 const Contact = () => {
-  const [mail, setMail] = useState("");
-  const [description, setDescription] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [phone, setPhone] = useState("");
+  const [formData, setFormData] = useState({
+    email: "",
+    description: "",
+    firstName: "",
+    lastName: "",
+    phone: "",
+  });
+
+  const [errors, setErrors] = useState<Errors>({});
+  interface Errors {
+    [key: string]: string;
+  }
+
+  const handleUserDataChange = (name: string, value: string) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    name: string
+  ) => {
+    handleUserDataChange(name, e.target.value);
+  };
+
+  const phoneRegex =
+    /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+
+  const validateForm = () => {
+    const validationSchema = Yup.object().shape({
+      firstName: Yup.string().required("Unesi ime!"),
+      lastName: Yup.string().required("Unesi prezime!"),
+      email: Yup.string()
+        .email("Unesi ispravni email")
+        .required("Unesi email adresu!"),
+      phone: Yup.string()
+        .test("is-phone_number", "Unesi ispravan broj telefona", (value) => {
+          if (value) {
+            return phoneRegex.test(value);
+          }
+          return true;
+        })
+        .typeError("Unesi ispravan broj telefona")
+        .required("Unesi broj telefona!"),
+      description: Yup.string().required("Unesi poruku!"),
+    });
+
+    try {
+      validationSchema.validateSync(formData, { abortEarly: false });
+      setErrors({} as typeof errors);
+      return true;
+    } catch (error) {
+      //catch variables have to be type of unknown
+      const validationErrors: { [key: string]: string } = {};
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      error.inner.forEach((err) => {
+        validationErrors[err.path] = err.message;
+      });
+      setErrors(validationErrors);
+      return false;
+    }
+  };
 
   const form = useRef();
-
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   async function sendEmail(e) {
     console.log("Pozivam se");
     e.preventDefault();
+    const isValid = validateForm();
 
-    if (mail === "" || description === "") {
-      notifyFailure("Unesite email i tekst poruke!");
-      return;
-    }
-
-    try {
-      await emailjs.sendForm(
-        "service_rididoi",
-        "template_a52kz4o",
-        form?.current,
-        "uEv5XssPSUZAjRkfW"
-      );
-      console.log("Uspjesno poslano");
-      notifySuccess("Podrška uspješno kontaktirana!");
-    } catch (err) {
-      console.log("Doslo je do pogreske");
-      notifyFailure("Došlo je do pogreške");
+    if (isValid) {
+      try {
+        await emailjs.sendForm(
+          "service_rididoi",
+          "template_a52kz4o",
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          form?.current,
+          "uEv5XssPSUZAjRkfW"
+        );
+        console.log("Uspjesno poslano");
+        notifySuccess("Podrška uspješno kontaktirana!");
+      } catch (err) {
+        console.log("Doslo je do pogreske");
+        notifyFailure("Došlo je do pogreške");
+      }
     }
   }
   return (
@@ -44,54 +105,60 @@ const Contact = () => {
       <div className=" bg-white-light w-full px-6 py-9 rounded-lg mb-2  z-10  flex flex-col lg:py-16 lg:gap-8 max-w-[1200px]">
         <form
           id="loginForm"
-          //onSubmit={handleSubmit}
           className="flex flex-col gap-3.5  lg:flex-row items-center"
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
           ref={form}
           onSubmit={sendEmail}
         >
           <div className="flex flex-col gap-5 w-full ">
             <Input
-              value={firstName}
-              name="first_name"
-              onChange={(e) => setFirstName(e.target.value)}
+              value={formData.firstName}
+              name="firstName"
+              onChange={(e) => handleInputChange(e, "firstName")}
               type="text"
               placeholder="Ime *"
+              error={errors.firstName}
             />
             <Input
-              value={lastName}
-              name="last_name"
+              value={formData.lastName}
+              name="lastName"
               type="text"
-              onChange={(e) => setLastName(e.target.value)}
+              onChange={(e) => handleInputChange(e, "lastName")}
               placeholder="Prezime *"
+              error={errors.lastName}
             />
 
             <Input
-              value={mail}
+              value={formData.email}
               name="email"
-              onChange={(e) => setMail(e.target.value)}
+              onChange={(e) => handleInputChange(e, "email")}
               type="text"
               placeholder="Email *"
+              error={errors.email}
             />
 
             <Input
-              value={phone}
+              value={formData.phone}
               type="text"
               name="phone"
-              onChange={(e) => setPhone(e.target.value)}
+              onChange={(e) => handleInputChange(e, "phone")}
               placeholder="Telefon *"
+              error={errors.phone}
             />
           </div>
 
           <div className="w-full h-full  ">
             <Textarea
-              // value={userData.password}
-              //  error={errors.password}
-              value={description}
+              value={formData.description}
               name="description"
-              onChange={(e) => setDescription(e.target.value)}
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              onChange={(e) => handleInputChange(e, "description")}
               placeholder="Poruka *"
               rows={"10"}
               cols="5"
+              error={errors.description}
             />
           </div>
         </form>
